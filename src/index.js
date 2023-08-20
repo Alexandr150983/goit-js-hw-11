81e02e46b1b78acf2d888765c6facc859ad082ec
Alexandr150983 <alexandr150983@gmail.com>
2023-08-20 15:12:15 +0300

    hw11 final

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
 
 let page = 1;
 let searchQuery = '';
-let isFirstSearch = true;
+let loadingImages = false;
+let totalHits = 0;
 
 const API_KEY = '38906051-b75951c25e9106fdf3cf1ae5b';
 const BASE_URL = 'https://pixabay.com/api/';
@@ -24,32 +24,44 @@ form.addEventListener('submit', async event => {
 
   page = 1;
   gallery.innerHTML = '';
-  loadMoreBtn.style.display = 'none';
 
   await fetchImages();
 });
 
-loadMoreBtn.addEventListener('click', fetchImages);
+window.addEventListener('scroll', async () => {
+  const scrollPosition = window.scrollY + window.innerHeight;
+  const pageHeight = document.body.scrollHeight;
+
+  if (scrollPosition >= pageHeight) {
+    await fetchImages();
+  }
+});
 
 async function fetchImages() {
+  if (loadingImages) {
+    return;
+  }
+
+  loadingImages = true;
+
   try {
-    const response =
-      await axios(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}
-        `);
+    const response = await axios(
+      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`
+    );
     const data = response.data;
 
     if (data.hits.length === 0) {
       Notiflix.Notify.warning(
-        'Sorry, there are no images matching your search query. Please try again.'
+      'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
 
-    if (isFirstSearch) {
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        isFirstSearch = false;
-    }
-      
+    if (page === 1) {
+        totalHits = data.totalHits;
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      }
+
     data.hits.forEach(image => {
       const card = document.createElement('a');
       card.href = image.largeImageURL;
@@ -57,36 +69,37 @@ async function fetchImages() {
       card.innerHTML = `
         <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy">
         <div class="info">
-            <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-            <p class="info-item"><b>Views:</b> ${image.views}</p>
-            <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-            <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+            <p class="info-item"><b>Likes</b> ${image.likes}</p>
+            <p class="info-item"><b>Views</b> ${image.views}</p>
+            <p class="info-item"><b>Comments</b> ${image.comments}</p>
+            <p class="info-item"><b>Downloads</b> ${image.downloads}</p>
         </div>
     `;
       gallery.appendChild(card);
     });
 
-    if (page < Math.ceil(data.totalHits / 40)) {
-      page = +1;
-      loadMoreBtn.style.display = 'block';
-    } else {
-      loadMoreBtn.style.display = 'none';
-      if (data.totalHits === 0) {
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-      }
-      const lightbox = new SimpleLightbox('.gallery a');
-      lightbox.refresh();
+    loadingImages = false;
+
+    const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
+
+      page += 1;
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+
   } catch (error) {
     console.error('Error fetching images:', error);
     Notiflix.Notify.failure(
       'Oops! Something went wrong while fetching images.'
     );
+
+    loadingImages = false;
   }
 }
